ff1b304d3cd6e2c12bdc7ebb67adc7fb721e89a4
Ray Wildman <rawildm@sandia.gov>
2023-05-11 12:44:30 -0600

    Checks remote for branch to merge with
    Gitlab runners don't seem to fetch all branches, so the previous check
    for the source branch wasn't working.

--- utilities/detail/merge-with-default-if-exists.sh
+++ utilities/detail/merge-with-default-if-exists.sh
@@ -3,10 +3,12 @@
 source ../utilities/detail/checkout-default-branch.sh
 
 BRANCH=${1}
-git show-ref --verify --quiet refs/heads/${BRANCH}
+git ls-remote --exit-code --heads origin "${BRANCH}"
 BRANCH_EXISTS=$?
 if [[ ${BRANCH_EXISTS} == 0 ]]
 then
     echo "Merging ${BRANCH} with default"
     git pull origin "${BRANCH}" --recurse-submodules
+else
+    echo "Did not find ${BRANCH} on remote, not merging"
 fi
