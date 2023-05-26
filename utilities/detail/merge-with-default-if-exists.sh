a6ac659df48914211439633d04f86ac82a6df584
Ray Wildman <rawildm@sandia.gov>
2023-05-26 12:05:28 -0600

    Fixes merge script

--- utilities/detail/merge-with-default-if-exists.sh
+++ utilities/detail/merge-with-default-if-exists.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 source ../utilities/detail/checkout-default-branch.sh
-DEFAULT_BRANCH=$(git branch --show-current)
 
 BRANCH=${1}
 git ls-remote --exit-code --heads origin "${BRANCH}"
 BRANCH_EXISTS=$?
 if [[ ${BRANCH_EXISTS} == 0 ]]
 then
-    echo "Merging ${BRANCH} with default branch ${DEFAULT_BRANCH}"
-    git fetch origin
-    git checkout "${BRANCH}"
-    git checkout "${DEFAULT_BRANCH}"
-    git merge "${BRANCH}"
+    echo "Merging ${BRANCH} with default branch"
+    git pull origin "${BRANCH}" --no-rebase --recurse-submodules --no-edit
 else
     echo "Did not find ${BRANCH} on remote, not merging"
 fi
