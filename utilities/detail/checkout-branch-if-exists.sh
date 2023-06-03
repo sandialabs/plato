a8657a64197c9b71e3bd75699fec77ba7cd33ea9
Ray Wildman <rawildm@sandia.gov>
2023-06-03 09:34:40 -0600

    Adds some more debug info

--- utilities/detail/checkout-branch-if-exists.sh
+++ utilities/detail/checkout-branch-if-exists.sh
@@ -7,8 +7,12 @@ if [[ ${BRANCH_EXISTS} == 0 ]]
 then
     git fetch origin
     git branch -v -a
+    echo "Fetching ${BRANCH}"
+    git fetch origin ${BRANCH}
+    git branch -v -a
+    git config --get remote.origin.fetch
     echo "Checking out ${BRANCH}"
-    git switch -c ${BRANCH}
+    git switch ${BRANCH}
     echo "Pulling ${BRANCH}"
     git pull --recurse-submodules
 fi
