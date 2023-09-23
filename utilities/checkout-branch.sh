42fb14c085ddf3d8df8cf484edf67d746c62b543
Ray Wildman <rawildm@sandia.gov>
2023-09-23 08:30:25 -0600

    Adds color to output

--- utilities/checkout-branch.sh
+++ utilities/checkout-branch.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+green="\033[1;32m"
+nc="\033[1;0m"
+
 if [[ -d "utilities" ]]
 then
   # Checkout main develop branches first
   source utilities/checkout-develop.sh
   # Checkout requested branch, if it fails, we'll remain on the main branch
   ./utilities/plato-submodule-foreach.sh "../utilities/detail/checkout-branch-if-exists.sh ${1}"
-  echo "Submodule are on the following branches:"
+  echo -e "\n${green}Submodules are on the following branches:${nc}"
   ./utilities/plato-submodule-foreach.sh "git branch --show-current"
 else
   echo "Can't find utilities directory. This script must be run from super-plato."
