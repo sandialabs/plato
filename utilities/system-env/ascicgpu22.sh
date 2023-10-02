0d2f404a37f815c128303d8be2f8af42c850f624
Ray Wildman <rawildm@sandia.gov>
2023-10-02 12:13:24 -0600

    Fixes cache path again

--- utilities/system-env/ascicgpu22.sh
+++ utilities/system-env/ascicgpu22.sh
@@ -5,4 +5,4 @@ module load cde/v3/git
 module load cde/v3/git-lfs
 module load ccache
 
-export RUNNER_HOME="/scratch/1540-runners/${GITLAB_USER_NAME}"
+export RUNNER_HOME="/scratch/1540-runners/${GITLAB_USER_LOGIN}"
