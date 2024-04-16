66a6a8ed33b856a7f7963c722111beb45797e4b6
Ray Wildman <rawildm@sandia.gov>
2024-04-16 11:48:38 -0600

    Adds leak suppression for stack-trace-less builds

--- utilities/test-env.sh
+++ utilities/test-env.sh
@@ -2,11 +2,14 @@
 
 if [[ -d "spack" ]]
 then
+  export SUPER_PLATO_ROOT=$(pwd)
+
   source spack/share/spack/setup-env.sh
   spack env activate .
   spack load cmake
   spack load platoengine
   spack load platoanalyze
+  source utilities/sanitizer-env.sh ${SUPER_PLATO_ROOT}/ci/detail/sanitizer_black_list.txt
 else
   echo "Can't find spack directory. This script must be run from the plato super project directory."
 fi
