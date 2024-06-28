c2a082f0b6bff0a48743077514bf84fd40c898eb
Ray Wildman <rawildm@sandia.gov>
2024-06-28 14:20:46 -0600

    Updates to fix-clang script for aue modules

--- utilities/compilers/add-module.sh
+++ utilities/compilers/add-module.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# Add binutils module
-MODULE_NAME=aue/binutils
+MODULE_NAME=${1}
 sed -i "s|modules: \[\]|modules: \[${MODULE_NAME}\]|" spack.yaml
--- utilities/compilers/cee-build.sh
+++ utilities/compilers/cee-build.sh
@@ -8,12 +8,12 @@ GCC_MODULE="aue/gcc/11.4.0"
 CLANG_MODULE="aue/clang/16.0.6"
 ./utilities/compilers/add-compiler.sh ${CLANG_MODULE} clang
 
-./utilities/compilers/add-binutils.sh
-
 GCC_DIR=$(./utilities/compilers/compiler-dir.sh ${GCC_MODULE} gcc)
 ./utilities/compilers/fix-clang-fortran.sh "${GCC_DIR}"
 
 ./utilities/compilers/find-external-package-from-module.sh ${CLANG_MODULE} clang llvm
+
+./utilities/compilers/add-module.sh ${GCC_MODULE}
 
 # cuda
 CUDA_MODULE="aue/cuda/11.8.0-gcc-10.3.0"
