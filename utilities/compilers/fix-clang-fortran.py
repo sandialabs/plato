2c913fbea4a6107d8803204387af4c1a28699f53
Ray Wildman <rawildm@sandia.gov>
2024-07-01 12:52:18 -0600

    Adds gcc toolchain to clang

--- utilities/compilers/fix-clang-fortran.py
+++ utilities/compilers/fix-clang-fortran.py
@@ -10,6 +10,8 @@ fflags = sys.argv[2]
 gfortran_path = os.path.join(gcc_base_dir, 'bin', 'gfortran')
 gfortran_lib_path = os.path.join(gcc_base_dir, 'lib64')
 
+gcc_toolchain_flag = "--gcc-toolchain=" + gcc_base_dir
+
 with open('spack.yaml') as stream:
   spack_yaml=yaml.safe_load(stream)
   for compiler in spack_yaml['spack']['compilers']:
@@ -18,7 +20,7 @@ with open('spack.yaml') as stream:
       compiler_obj['paths']['f77'] = gfortran_path
       compiler_obj['paths']['fc'] = gfortran_path
       compiler_obj['extra_rpaths'] = [gfortran_lib_path]
-      compiler_obj['flags'] = {'fflags': fflags}
+      compiler_obj['flags'] = {'fflags': fflags, 'cflags': gcc_toolchain_flag, 'cppflags': gcc_toolchain_flag}
 
 with open('spack.yaml', 'w') as stream:
   yaml.dump(spack_yaml, stream)
