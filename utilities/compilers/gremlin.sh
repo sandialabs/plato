66247311ffb3940ecbeddd2ed3d3e69844f568c2
Ray Wildman <rawildm@sandia.gov>
2024-09-16 12:56:57 -0600

    Fixes gfortran path

--- utilities/compilers/gremlin.sh
+++ utilities/compilers/gremlin.sh
@@ -2,5 +2,5 @@
 
 # Find clang
 spack compiler find /usr/bin
-./utilities/compilers/fix-clang-fortran.py "/usr/bin" compilers_only
+./utilities/compilers/fix-clang-fortran.py "/usr" compilers_only
 spack external find --not-buildable --scope env:$(pwd -P) llvm
