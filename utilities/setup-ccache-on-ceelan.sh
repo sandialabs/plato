4ec523dc14efa766a8134ae340d097b90f593ea5
Ray Wildman <rawildm@sandia.gov>
2024-10-28 14:00:17 -0600

    Adds check that user is in ccache dir group

--- utilities/setup-ccache-on-ceelan.sh
+++ utilities/setup-ccache-on-ceelan.sh
@@ -2,6 +2,11 @@
 
 CEELAN_CCACHE_DIR=/projects/plato/automated-builds/ccache
 if [ -d "$CEELAN_CCACHE_DIR" ]; then
-  export CCACHE_DIR=${CEELAN_CCACHE_DIR}
-  echo "Setting ccache dir to ${CCACHE_DIR}"
+  CCACHE_DIR_GROUP=$(stat -c "%G" $CEELAN_CCACHE_DIR)
+  id -nG "$USER" | grep -qw "$CCACHE_DIR_GROUP"
+  USER_IN_PROJECTS_GROUP=$?
+  if [ "$USER_IN_PROJECTS_GROUP" = "0" ]; then
+    export CCACHE_DIR=${CEELAN_CCACHE_DIR}
+    echo "Setting ccache dir to ${CCACHE_DIR}"
+  fi
 fi
