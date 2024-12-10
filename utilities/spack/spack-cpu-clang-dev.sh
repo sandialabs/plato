2e759c39c5091dad23a141a33367ac4298febb21
ralberd <ralberd@sandia.gov>
2024-12-10 15:33:59 -0700

    getting rid of trilinos~mumps in specs since it is not set in the platoengine spack package

--- utilities/spack/spack-cpu-clang-dev.sh
+++ utilities/spack/spack-cpu-clang-dev.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ARCH_ARG=${1}
 spack add platoanalyze@develop%clang~amgx~cuda+tacho+enginemesh+esp+mpmd+python+dev_build build_type=Debug ${SYSTEM_ARCH} \
-  ^trilinos~mumps \
   ^openmpi@4.1.3 \
   ^platoengine@develop%clang+esp+expy+iso+platoproxy+prune+stk+unit_testing+dev_build build_type=Debug
 
--- utilities/spack/spack-cpu-clang.sh
+++ utilities/spack/spack-cpu-clang.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 spack add platoanalyze@develop%clang~amgx~cuda+tacho+enginemesh+esp+mpmd+python build_type=RelWithDebInfo ${SYSTEM_ARCH} \
-  ^trilinos~mumps \
   ^openmpi@4.1.3 \
   ^platoengine@develop%clang+esp+expy+iso+platoproxy+prune+stk+unit_testing build_type=RelWithDebInfo
 
