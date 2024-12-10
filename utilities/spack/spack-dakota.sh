4c6fdd8031ceee92a57602b2bf9cf12916bdd2a0
ralberd <ralberd@sandia.gov>
2024-12-10 13:12:44 -0700

    removes ~mumps trilinos spec for dakota

--- utilities/spack/spack-dakota.sh
+++ utilities/spack/spack-dakota.sh
@@ -3,5 +3,4 @@
 spack add platoanalyze@develop~amgx~cuda+dakota_tests+enginemesh+esp+mpmd+tacho ${SYSTEM_ARCH} \
   ^dakota cxxflags="-fno-new-ttp-matching" fflags="-fallow-argument-mismatch" \
   ^openmpi@4.1.3 \
-  ^platoengine@develop+dakota+esp~expy+sierra_tests \
-  ^trilinos~mumps
+  ^platoengine@develop+dakota+esp~expy+sierra_tests
