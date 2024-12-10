7ae6c06e5ecbd72b53c02df2416d9b6074517719
ralberd <ralberd@sandia.gov>
2024-12-10 13:17:04 -0700

    explicitly spec trilinos to have no krino

--- utilities/spack/spack-dakota.sh
+++ utilities/spack/spack-dakota.sh
@@ -3,4 +3,5 @@
 spack add platoanalyze@develop~amgx~cuda+dakota_tests+enginemesh+esp+mpmd+tacho ${SYSTEM_ARCH} \
   ^dakota cxxflags="-fno-new-ttp-matching" fflags="-fallow-argument-mismatch" \
   ^openmpi@4.1.3 \
-  ^platoengine@develop+dakota+esp~expy+sierra_tests
+  ^platoengine@develop+dakota+esp~expy+sierra_tests \
+  ^trilinos~mumps~stk~krino
