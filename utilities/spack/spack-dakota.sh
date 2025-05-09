724696ee3de5f2b9b5aef0da5795c09408b408cf
Ray Wildman <rawildm@sandia.gov>
2025-05-09 12:41:58 -0600

    Updgrades openmpi to 4.1.6

--- utilities/spack/spack-dakota.sh
+++ utilities/spack/spack-dakota.sh
@@ -2,6 +2,6 @@
 
 spack add platoanalyze@develop~amgx~cuda+dakota_tests+enginemesh+esp+mpmd+tacho ${SYSTEM_ARCH} \
   ^dakota cxxflags="-fno-new-ttp-matching" fflags="-fallow-argument-mismatch" \
-  ^openmpi@4.1.3 \
+  ^openmpi@4.1.6 \
   ^platoengine@develop+dakota+esp~expy+sierra_tests \
   ^trilinos~mumps~stk~krino
