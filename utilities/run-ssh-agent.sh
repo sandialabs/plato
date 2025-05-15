a9672f5a92e8daa6173852d9781d7c0c23eed740
Ray Wildman <rawildm@sandia.gov>
2025-05-15 09:41:00 -0600

    Fixes multiple ssh-agent process running on cee-lan

--- utilities/run-ssh-agent.sh
+++ utilities/run-ssh-agent.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-RUN_COUNT=$(ps aux | grep [s]sh-agent | grep -v run-ssh-agent | grep gitlab | wc -l)
-echo "Found $RUN_COUNT ssh-agent procs"
+SHORT_USER=$(echo $USER | cut -b 1-7)
+RUN_COUNT=$(ps aux | grep [s]sh-agent | grep -v run-ssh-agent | grep ${SHORT_USER} | wc -l)
+echo "Found $RUN_COUNT ssh-agent procs for user $SHORT_USER"
 if [ $RUN_COUNT -lt 1 ] ; then
   echo "Starting ssh-agent"
   eval `ssh-agent`
 else
   echo "ssh-agent is running"
 fi
-
