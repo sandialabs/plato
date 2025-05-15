e0cb4ff0210f9fc1268b7c3c4b003e692b71b238
Ray Wildman <rawildm@sandia.gov>
2025-05-15 07:52:24 -0600

    Adds cuda 12 module

--- utilities/compilers/cee-build.sh
+++ utilities/compilers/cee-build.sh
@@ -18,6 +18,8 @@ CLANG_GFORTRAN_FLAGS="-fPIC"
 ./utilities/compilers/find-external-package-from-module.sh ${CLANG_MODULE} clang llvm
 
 # cuda
-CUDA_MODULE="aue/cuda/11.8.0-gcc-10.3.0"
-./utilities/compilers/find-external-package-from-module.sh ${CUDA_MODULE} nvcc cuda
+CUDA_11_MODULE="aue/cuda/11.8.0-gcc-10.3.0"
+./utilities/compilers/find-external-package-from-module.sh ${CUDA_11_MODULE} nvcc cuda
+CUDA_12_MODULE="aue/cuda/12.4.0-gcc-12.3.0"
+./utilities/compilers/find-external-package-from-module.sh ${CUDA_12_MODULE} nvcc cuda
 
