7ee291bd1a43ba7aa3f381bed926ba82f1d86d84
Sean Hardesty <shardes@sandia.gov>
2025-07-21 18:16:02 -0600

    remove deleted variants

--- utilities/spack/spack-cpu-snopt.sh
+++ utilities/spack/spack-cpu-snopt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ARCH_ARG=${1}
-spack add platoanalyze@develop%clang~amgx~cuda+tacho+enginemesh+esp+mpmd+python+dev_build build_type=Debug ${SYSTEM_ARCH} \
+spack add platoanalyze@develop%clang~amgx~cuda+tacho+enginemesh+dev_build build_type=Debug ${SYSTEM_ARCH} \
   ^openmpi@4.1.6 \
   ^platoengine@develop%clang+esp+expy+iso+platoproxy+prune+stk+unit_testing+dev_build+snopt build_type=Debug
 
--- utilities/spack/spack-dakota.sh
+++ utilities/spack/spack-dakota.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-spack add platoanalyze@develop~amgx~cuda+dakota_tests+enginemesh+esp+mpmd+tacho ${SYSTEM_ARCH} \
+spack add platoanalyze@develop~amgx~cuda+dakota_tests+enginemesh+tacho ${SYSTEM_ARCH} \
   ^dakota cxxflags="-fno-new-ttp-matching" fflags="-fallow-argument-mismatch" \
   ^openmpi@4.1.6 \
   ^platoengine@develop+dakota+esp~expy+sierra_tests \
