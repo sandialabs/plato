347094fafb28252a7c3ab77f1e39db6719ee606e
Ray Wildman <rawildm@sandia.gov>
2025-10-14 13:44:50 -0600

    Updates dev_build variant

--- utilities/spack/spack-cpu-clang-dev.sh
+++ utilities/spack/spack-cpu-clang-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-spack add platoanalyze@develop~amgx~cuda+tacho+enginemesh+dev_build build_type=Debug ${SYSTEM_ARCH} \
-  ^platoengine@develop+esp+unit_testing+dev_build build_type=Debug \
+spack add platoanalyze@develop~amgx~cuda+tacho+enginemesh+dev_build  build_type=Debug ${SYSTEM_ARCH} \
+  ^platoengine@develop+esp+unit_testing cmake_preset=dev_build build_type=Debug \
   ^openmpi@4.1.6 
