f3a954a4d28d39e91bce9155f2d1b1758d9e3303
Ray Wildman <rawildm@sandia.gov>
2025-10-21 09:36:58 -0600

    Adds umfpack to CI specs

--- utilities/spack/spack-cpu-clang-dev.sh
+++ utilities/spack/spack-cpu-clang-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-spack add platoanalyze@develop~amgx~cuda+tacho+enginemesh cmake_preset=dev_build build_type=Debug ${SYSTEM_ARCH} \
+spack add platoanalyze@develop~amgx~cuda+tacho+umfpack+enginemesh cmake_preset=dev_build build_type=Debug ${SYSTEM_ARCH} \
   ^platoengine@develop+esp+unit_testing cmake_preset=dev_build build_type=Debug \
   ^openmpi@4.1.6 
--- utilities/spack/spack-cpu-clang.sh
+++ utilities/spack/spack-cpu-clang.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-spack add platoanalyze@develop~amgx~cuda+tacho+enginemesh build_type=RelWithDebInfo ${SYSTEM_ARCH} \
+spack add platoanalyze@develop~amgx~cuda+tacho+umfpack+enginemesh build_type=RelWithDebInfo ${SYSTEM_ARCH} \
   ^platoengine@develop+esp+unit_testing build_type=RelWithDebInfo \
   ^openmpi@4.1.6 
